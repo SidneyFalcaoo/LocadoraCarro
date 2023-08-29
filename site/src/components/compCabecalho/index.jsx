a9d7e58ed2e5ab52f6d4ddd97ba3cf56dd4e8a98
Suyndara <souzamatheussilva41@gmail.com>
2023-08-29 17:31:45 -0300

    ok

---
+++ site/src/components/compCabecalho/index.jsx
@@ -0,0 +1,17 @@
+import './index.scss';
+
+export default function Cabecalho() {
+    
+
+    return(
+        <div className='comp-cabecalho'>
+            <nav>
+                <img src="/assets/img/logo.png" alt="" />
+                <h1>Elite<strong>Wheelz</strong></h1>
+            </nav>
+
+
+
+        </div>
+    )
+}
---
+++ site/src/pages/index.jsx
@@ -0,0 +1,11 @@
+import './index.scss';
+import Cabecalho from '../components/compCabecalho';
+
+export default function Home() {
+
+    return(
+        <div className='pagina-home'>
+            <Cabecalho/>
+        </div>
+    )
+}
