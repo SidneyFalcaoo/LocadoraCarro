8264627d802086769c516392167ea937a1343458
SidneyFalcaoo <sidneyoliveirafalcaooo@gmail.com>
2023-08-29 17:42:33 -0300

    OK

---
+++ api/src/index.js
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+import ClienteController from '../src/Controller/ClienteController.js'
+
+let servidor = express();
+servidor.use(cors());
+servidor.use(express.json());
+
+servidor.use(ClienteController);
+
+servidor.listen(process.env.PORT, () => console.log(`Api subiu na porta ${process.env.PORT}`));
