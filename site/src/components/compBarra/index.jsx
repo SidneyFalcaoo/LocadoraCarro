cccf76c671f002f26241bd826a21f84acb063218
Suyndara <souzamatheussilva41@gmail.com>
2023-08-29 18:28:45 -0300

    ok

---
+++ site/src/components/compBarra/index.jsx
@@ -0,0 +1,10 @@
+import './index.scss';
+
+export default function Barra() {
+
+    return(
+        <div className='comp-barra'>
+            <h1>Olá, que bom que você voltou!</h1>
+        </div>
+    )
+}
--- site/src/pages/index.jsx
+++ site/src/pages/index.jsx
@@ -1,13 +1,13 @@
 import './index.scss';
 import Cabecalho from '../components/compCabecalho';
+import Barra from '../components/compBarra';
 
 export default function Home() {
 
     return(
         <div className='pagina-home'>
             <Cabecalho/>
-
-            
+            <Barra/>
         </div>
     )
 }
