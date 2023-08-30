29f2acba066f51567ca9e2c9fbad625fe76abbe5
Suyndara <souzamatheussilva41@gmail.com>
2023-08-29 22:30:41 -0300

    ok

--- site/src/pages/index.jsx
+++ site/src/pages/index.jsx
@@ -7,7 +7,19 @@ export default function Home() {
     return(
         <div className='pagina-home'>
             <Cabecalho/>
-            <Barra/>
+
+            <main className='principarte'>
+                <Barra/>
+
+                <article className='s1'>
+                    <p><strong>Área administrativa</strong></p>
+                    <h1>Controle de Clientes</h1>
+                </article>
+
+                <section className='s2'>
+                    <h1>Novo cliente</h1>
+                </section>
+            </main>
         </div>
     )
 }
