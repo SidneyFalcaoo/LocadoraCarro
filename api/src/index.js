5539db3029bbc683cc8aebc96d9f9843f32e8693
SidneyFalcaoo <sidneyoliveirafalcaooo@gmail.com>
2023-08-30 17:27:58 -0300

    OK

--- api/src/index.js
+++ api/src/index.js
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 
-import veiculoController from './controller/veiculoController.js';
+import veiculoController from './Controller/VeiculoController.js';
 import clienteController from './controller/clienteController.js'
 
 let servidor = express();
